8d038fca8d77bcf213e85988b6a1dae0865d2720
Brienyll <Brienyll29@gmail.com>
2019-08-05 23:12:03 -0700

    fixed board

--- src/Board.js
+++ src/Board.js
@@ -31,10 +31,20 @@ import './Board.css';
 
 class Board extends Component {
 
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25
+  };
+
   constructor(props) {
     super(props);
 
     // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    };
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -42,6 +52,13 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn)
+      }
+      board.push(row);
+    }
     return board
   }
 
@@ -74,15 +91,7 @@ class Board extends Component {
 
   render() {
     return (
-      <table className="Board">
-        <tbody>
-          <tr>
-            <Cell isLit={true}/>
-            <Cell isLit={false}/>
-            <Cell isLit={true}/>
-          </tr>
-        </tbody>
-      </table>
+      <h1>Lights off </h1>
     )
     // if the game is won, just show a winning msg & render nothing else
 
